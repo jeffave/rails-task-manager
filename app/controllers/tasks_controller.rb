7fb0591cddc25afaf2cbcd5ccc8eca6daea9c40c
Jefferson Avelino <jefferson.alves@msn.com>
2021-11-16 20:02:36 -0300

    Parcially done.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,14 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+ def new
+   @task = Task.new
+ end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+end
